97c6253ab56f5bdf86de7f416860fda7d1b9d13b
MrZaku <110213251+MrZaku@users.noreply.github.com>
2025-03-08 09:31:57 +0500

    Update Selection_Sort.cpp

--- Selection_Sort.cpp
+++ Selection_Sort.cpp
@@ -1,32 +1,37 @@
 #include<iostream>
-#include <cstdlib>
 using namespace std;
-int main(){
-	int n=10;
-	int arr[n];
-	for(int i=0; i<n; i++){
-		int a=rand()/100;
-		arr[i]=a;
-	}
-	for(int i=0; i<n; i++){
-		cout<<arr[i]<<"  ";
-	}	
-	cout<<endl;
-	
-	//selection sort
+
+void selectionsort(int arr[], int n){
 	for(int i=0; i<n; i++){
 		int min = i;
 		for(int j=i+1; j<n; j++){
-			if (arr[j]<arr[min]){
-				min = j;
-			}
-		}
+			if(arr[j]<arr[min])
+				min = j;}
 		swap(arr[i], arr[min]);
 	}
-	
+}
+
+void insert(int arr[], int n){
 	for(int i=0; i<n; i++){
-		cout<<arr[i]<<"  ";
-	}
-	
+		cin>>arr[i];}
+}
+
+void display(int arr[], int n){
+	for(int i=0; i<n; i++){
+		cout<<arr[i]<<"  ";}
+		cout<<endl;
+}
+
+int main(){
+	int n;
+	cout<<"Enter the size of array; ";
+	cin>>n;
+	int arr[n];
+	insert(arr, n);
+	cout<<"\nBefore sorting; ";
+	display(arr, n);
+	selectionsort(arr, n);
+	cout<<"\nAfter sorting; ";
+	display(arr, n);
 	return 0;
 }
